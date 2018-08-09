17b98b7e671ffe3e78c07955518ced87a0498d97
Tiberio Padilha <tiberio_padilha@hotmail.com>
2018-08-09 10:50:40 -0300

    Criando operação de instanciacao

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.nelioalves.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{ //interface que faz com que a classe execute toda vez que rodar a aplicação
+	
+	@Autowired 
+	private CategoriaRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception { //metodo da interface executado ao rodar a aplicação
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepository.save(cat1);
+		catRepository.save(cat2);
+		
 	}
 }
